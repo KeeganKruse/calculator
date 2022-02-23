eba76d359471946ed9ba3083da7180576037d002
Keegan Kruse <krusekeegan@gmail.com>
2022-02-22 19:30:27 -0700

    first commit

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+const { ReadableStreamBYOBRequest } = require('stream/web');
+const { callbackify } = require('util');
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -12,7 +14,22 @@ reader.question("What would you like to calculate?", function(input){
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+	num2 = Number(tokens[2] || 0);
+    console.log('mathSymbol', mathSymbol);
+    console.log('num1', num1);
+    console.log('num2', num2)
+
+
+if (mathSymbol === "+"){console.log(num1 + num2);}
+
+if (mathSymbol === "-"){console.log(num1 - num2);}
+
+if (mathSymbol === "*"){console.log(num1 * num2);}
+
+if (mathSymbol === "/"){console.log(num1 / num2);}
+
+if (mathSymbol === "&"){console.log(Math.sqrt(num1));}
+
 
 
 	// This line closes the connection to the command line interface.
